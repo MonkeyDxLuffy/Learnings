d2fbb9fc0f1797f207d9fae808dcd011139ebff8
ANISH <anishmehta0000@gmail.com>
2025-09-28 18:22:45 +0530

    dom selectors only

---
+++ js Learning/Js Basic/Dom Manipulation/script.js
@@ -0,0 +1,12 @@
+const h1tag = document.getElementById('header');
+const button = document.getElementsByClassName('btn');
+const Paragraph = document.querySelector('p');
+const allLinks = document.querySelectorAll('a');
+
+
+// id will give proper id 
+// class name will give list 
+// queryselector will 1st or perticular query 
+// queryselector all will give list like [0][1] and so one 
+
+console.log()
